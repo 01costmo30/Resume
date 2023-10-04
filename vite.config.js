b53240093a7209f83a52175c9b87fac181f9ff94
01costmo30 <01lu301998@gmail.com>
2023-10-04 10:54:57 +0800

    fix route

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '',
+  base: '/Resume',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
